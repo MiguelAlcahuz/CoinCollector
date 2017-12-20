c6335c3e71c682276f80dedfa9dcbd3bdb762b1c
MiguelAM <malcahuz@alfatecsistemas.es>
2017-12-20 11:25:25 +0100

    Added more details to entity

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -2,14 +2,58 @@ class Coin < ApplicationRecord
 	validates :year, presence: true
 	validates :value, presence: true
 	
+	# COUNTRIES WITH EURO AS THEIR CURRENCY
 
-
-	yearArray = Array.new
-	for i in 1999..Date.current.year
-		yearArray << i.to_s
-	end
+	ANDORRA = 0 # introduced in 2015
+	AUSTRIA = 1 # introduced in 1999
+	BELGIUM = 2 # introduced in 1999
+	CIPURS = 3 # introduced in 2008
+	ESTONIA = 4 # introduced in 2011
+	FINLAND = 5 # introduced in 1999
+	FRANCE = 6 # introduced in 1999
+	GERMANY = 7 # introduced in 1999
+	GREECE = 8	# introduced in 2001
+	IRELAND = 9 # introduced in 1999
+	ITALY = 10 # introduced in 1999
+	LATVIA = 11 # introduced in 2014
+	LITHUANIA = 12 # introduced in 2015
+	LUXEMBURG = 13 # introduced in 1999
+	MALTA = 14 # introduced in 2007
+	MONACO = 15 # introduced in 1999
+	NETHERLANDS = 16 # introduced in 1999
+	PORTUGAL = 17 # introduced in 1999
+	SANMARINO = 18 # introduced in 1999
+	SLOVAKIA = 19 # introduced in 2008
+	SLOVENIA = 20 # introduced in 2007
+	SPAIN = 21 # introduced in 1999
+	VATICAN = 22 # introduced in 1999
+	
 
 	enum country: {
+
+		andorra: ANDORRA,
+		austria: AUSTRIA,
+		belgium: BELGIUM, 
+		ciprus: CIPRUS,	
+		estonia: ESTONIA, 
+		finland: FINLAND, 
+		france: FRANCE, 
+		germany: GERMANY, 
+		greece: GREECE,	
+		ireland: IRELAND, 
+		italy: ITALY, 
+		latvia: LATVIA, 
+		lithuania: LITHUANIA, 
+		luxemburg: LUXEMBURG, 
+		malta: MALTA, 
+		monaco: MONACO,
+		netherlands: NETHERLANDS,	
+		portugal: PORTUGAL, 
+		sanmarino: SANMARINO,
+		slovakia: SLOVAKIA, 
+		slovenia: SLOVENIA, 
+		spain: SPAIN, 
+		vatican: VATICAN
 
 	}
 
