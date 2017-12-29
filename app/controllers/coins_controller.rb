6ce9594888da852bb4d6955ea1c4d0763ae0aba1
MiguelAM <malcahuz@alfatecsistemas.es>
2017-12-29 13:29:21 +0100

    add details to coin table

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -63,6 +63,16 @@ class CoinsController < ApplicationController
     end
   end
 
+    def select_options_tag(name='',select_options={},options={})
+    #set selected from value
+    selected = ''
+    unless options[:value].blank?
+      selected = options[:value]
+      options.delete(:value)
+    end
+    select_tag(name,options_for_select(select_options,selected),options)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
