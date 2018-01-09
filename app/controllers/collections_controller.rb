77b5d3f736a824d09440ab1f64ef14a813cbab0b
MiguelAM <malcahuz@alfatecsistemas.es>
2018-01-09 11:16:27 +0100

    Add registration

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,5 +1,6 @@
 class CollectionsController < ApplicationController
   before_action :set_collection, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /collections
   # GET /collections.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
 
+  devise_for :users
   resources :collections do
   	resources :coins 
   end
