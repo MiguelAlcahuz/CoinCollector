08fdd97d6e5138c9c17162bd58f8d2b2c0c237d1
MiguelAM <malcahuz@alfatecsistemas.es>
2018-01-11 10:21:17 +0100

    fix login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,  :authentication_keys => { login: false, email: true }
+         :recoverable, :rememberable, :trackable, :validatable,  :authentication_keys => [:login]
   def login=(login)
   	@login = login
   end
