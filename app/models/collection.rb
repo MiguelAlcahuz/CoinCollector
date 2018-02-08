815c1b5f4baca0411741200ffcd714803a7dd93a
MiguelAM <malcahuz@alfatecsistemas.es>
2018-02-08 09:50:27 +0100

    add all test for collection model and policy, and fix routing test for user

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,6 +2,15 @@ class Collection < ApplicationRecord
   has_and_belongs_to_many :coins
   belongs_to :user
   validates :currency, presence: true, uniqueness: true
+  validate :collection_validator
+
+  def id_validation?
+  	user_id == user&.id 
+  end
+  	
+  def collection_validator
+  	errors.add(:user_id, "not the user of this collection") unless id_validation?
+  end
 
   def my_owner_username
   	self.user.username
--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -14,11 +14,38 @@ RSpec.describe Collection, type: :model do
         expect(c.my_owner_username).to eql("H")
       end
     end
+  end
 
-    context 'when the collection is correctly' do
-      it 'returns true' do
-      	c = Collection.create(currency: "euros", user_id: 5)
+  describe "#validations" do
+    context 'when the collection is correctly created' do
+      it 'returns true when assigned to the correct user' do
+      	u = User.create(id:7, email: "miguel@gmail.com", password: "12341234", username: "H")
+      	c = Collection.new(currency: "eur", user_id: 7)
+        expect(c.id_validation?).to be true
+      end
+    end
+
+    context 'when the collection is correctly created' do
+      it 'returns false when not assigned to the correct user' do
+      	u = User.create(id:6, email: "migueldasdasd@gmail.com", password: "12341234", username: "Hasasdsda")
+      	c = Collection.new(currency: "euasdasdr", user_id: 7)
+        expect(c.id_validation?).to be false
+      end
+    end
+
+     context 'when the collection is correctly created' do
+      it 'returns true when assigned to the correct user' do
+      	u = User.create(id:7, email: "miguel@gmail.com", password: "12341234", username: "H")
+      	c = Collection.create(id: 10, currency: "eur", user_id: 7)
         expect(c.valid?).to be true
+      end
+    end
+
+    context 'when the collection is correctly created' do
+      it 'returns false when not assigned to the correct user' do
+      	u = User.create(id:6, email: "miguel@gmail.com", password: "12341234", username: "H")
+      	c = Collection.create(id: 10, currency: "eur", user_id: 7)
+        expect(c.valid?).to be false
       end
     end
   end
